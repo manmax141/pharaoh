2580586f8ccbe1ad2fee82dc90e783f508e35275
manmax141 <68015857+manmax141@users.noreply.github.com>
2024-04-21 12:38:25 +0200

    Update pharaohformarwan.lua

--- pharaohformarwan.lua
+++ pharaohformarwan.lua
@@ -286,7 +286,10 @@ local s, e = pcall(function()
             UI.Watermark.Visible = false
             UI.Parent = gethui()
             
+            UI.Size = UDim2.new(0.175, 0, 0.265, 0);
+            
             game:GetService("UserInputService").InputBegan:Connect(function(i,p)
+                if p then return; end
                 if i.KeyCode == Enum.KeyCode.H then
                     UI.Enabled = not UI.Enabled
                 end
